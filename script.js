9b60b1c3a84cb9b178cd3b968c6ef811b361d8ca
Jamila <135280536+potpot89@users.noreply.github.com>
2024-03-03 12:43:00 +0100

    added google graph

--- script.js
+++ script.js
@@ -63,11 +63,11 @@ let body = document.getElementsByTagName(`body`);
 
 //variable for the mock file for development purpose. It fetches the json file we created and it allows to work offline.
 //it allows us to work on fake data / signposts
-let astronomyPictures = "mock/astronomy-pictures.json";
+//let astronomyPictures = "mock/astronomy-pictures.json";
 
 //URL to the endpoint with the queries and api key - for final project that goes to the online endpoint - comment the mock variable and comment out below fetch API inquiry, to move from the mock to the actual API app
 
-//let astronomyPictures = `https://api.nasa.gov/planetary/apod?start_date=${start}&end_date=${end}&api_key=${apiKey}&thumbs=true`; //thumbs= true enables the thumbnails url, which gives a preview of a video, in case there is a video instead of an img, using a conditional
+let astronomyPictures = `https://api.nasa.gov/planetary/apod?start_date=${start}&end_date=${end}&api_key=${apiKey}&thumbs=true`; //thumbs= true enables the thumbnails url, which gives a preview of a video, in case there is a video instead of an img, using a conditional
 
 //assegno fetch a una variabile. in questo caso fetch è una funzione che al suo interno assegna un'altra fetch.
 let fetchPictures = () => {
@@ -175,8 +175,119 @@ let fetchOtherPictures = fetchPictures().then((otherPictures) => {
     });
   });
 });
+
+//create variable for the modal window to handle it
+let pictureDetailsContainer = document.querySelector(`#details-container`);
+
 //close modal window setting it back to display none
 let closeButton = document.querySelector(`#close-button`);
 closeButton.addEventListener(`click`, () => {
-  document.querySelector(`#details-container`).style.display = `none`;
+  pictureDetailsContainer.style.display = `none`;
 });
+
+// close the modal window when clicking outside of it
+window.addEventListener("click", (e) => {
+  if (e.target == pictureDetailsContainer) {
+    document.querySelector(`#details-container`).style.display = `none`;
+  }
+});
+
+// Close it when pressing ESC
+window.addEventListener("keydown", (e) => {
+  if (e.key === "Escape") {
+    pictureDetailsContainer.style.display = "none";
+  }
+});
+
+/* ----- Dati in arrivo da Marte! ----- */
+
+// Dati di Curiosity: https://mars.nasa.gov/msl/weather/
+// Google line charts: https://developers.google.com/chart/interactive/docs/gallery/linechart
+
+const curiosityData =
+  "https://mars.nasa.gov/rss/api/?feed=weather&category=msl&feedtype=json";
+
+let fetchCuriosityData = () => {
+  let data = fetch(curiosityData)
+    .then((res) => res.json())
+    .then((data) => data.soles);
+  return data;
+};
+
+fetchCuriosityData()
+  .then((res) => {
+    let marsWeatherData = [];
+    let martianYear = 668;
+    // Riempiamo un array
+    for (let i = 0; i < martianYear; i++) {
+      marsWeatherData.push(res[i]);
+    }
+    return marsWeatherData;
+  })
+  .then((data) => {
+    // console.log(data);
+    let todayWeather = data[0];
+    // console.log(today)
+    document.querySelector("#mars-today").innerHTML = `
+      <p>Hi from Curiosity!<br>
+      This is my <strong>${todayWeather.sol}</strong> Martian day.<br>
+      Today it's <strong>${todayWeather.atmo_opacity}</strong>, min temp is 
+      <strong>${todayWeather.min_temp}°</strong> and max temp is 
+      <strong>${todayWeather.max_temp}°</strong>.</p>
+    `;
+    // Step
+    // caricare la chart di Google DOPO aver inserito il link nello script della head
+    google.charts.load("current", { packages: ["corechart"] });
+    google.charts.setOnLoadCallback(() => {
+      myChart(data);
+    });
+  });
+
+let myChart = (weatherData) => {
+  // console.log(weatherData);
+
+  // formattare i dati
+  // dall'array marsWeatherData prendiamo 3 proprietà: giorni, max e min temp.
+  // [
+  //   [1235, -75, 10],
+  //   [1236, -55, 14],
+  //   [1237, -35, 8],
+  //   ...
+  // ]
+  // riempiamo la chart con i valori dell'array "pulito"
+  let formattedData = weatherData.map((data) => {
+    return [data.sol, +data.min_temp, +data.max_temp];
+  });
+
+  var chartData = [["Date", "Min", "Max"]];
+
+  formattedData = formattedData.reverse();
+  for (let data of formattedData) {
+    chartData.push(data);
+  }
+
+  let options = {
+    title: "Mars Weather data",
+    // curveType: 'function',
+    hAxis: {
+      title: "Sols",
+      titleTextStyle: {
+        color: "#0000FF",
+      },
+    },
+    vAxis: {
+      title: "Temp (Celsius)",
+      ticks: [-80, -60, -40, -20, 0, 10],
+    },
+    legend: {
+      position: "bottom",
+    },
+  };
+
+  let finalData = new google.visualization.arrayToDataTable(chartData);
+  // ci agganciamo a un div nel DOM per disegnare la chart
+  let chart = new google.visualization.LineChart(
+    document.getElementById("mars-data")
+  );
+  chart.draw(finalData, options);
+};
